b18f8b83a6701937a854207e3994f77d0432a9a9
justanr <alecreiter@gmail.com>
2015-03-16 17:48:33 -0400

    Prolly gonna use this too.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="gonads",
+    version="0.0.1",
+    author="Alec Nikolas Reiter",
+    description="Bad monads for Python",
+    license="MIT",
+    packages=["gonads"]
+    )
