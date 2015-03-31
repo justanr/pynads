fe50830ef5a0637dc09b8643407d65def24fb41e
justanr <alecreiter@gmail.com>
2015-03-31 10:05:55 -0400

    Expanded maybe documentation.

--- pynads/concrete/maybe.py
+++ pynads/concrete/maybe.py
@@ -15,6 +15,81 @@ class Maybe(Monad):
 
     The unit method returns an instance of Just, which is the minimal
     context needed for a potential computation.
+
+    Consider the Haskell implementation of Maybe:
+
+    .. code-block: Haskell
+       data Maybe a = Nothing | Just a
+
+       instance Functor Maybe where
+           fmap f Nothing   = Nothing
+           fmap f (Just x)  = Just (f x)
+
+       instance Applicative Maybe where
+           pure             = Just
+           Nothing <*> _    = Nothing
+           (Just f) <*> x   = fmap f x
+
+       instance Monad Maybe where
+           return           = Just
+           Nothing  >>=     = Nothing
+           (Just x) >>= f   = f x
+
+    Doing any of these actions to a Nothing results in Nothing -- like
+    Billy Preston sang:
+
+        Nothing from nothing leaves nothing
+        You gotta have something if you wanna be with me
+        Nothing from nothing leaves nothing
+        You gotta have something if you wanna be with me
+
+    However, mapping over a Just, returns the result wrapped in Just.
+    Applying a function wrapped in Just maps it over the next value. And
+    binding a Just a with a function, returns the result of the function,
+    which should be a Maybe -- the function is responsible for determining
+    if it should be a Nothing or (Just x).
+
+    Maybe is helpful when you expect you'll encounter a None in Python.
+    In fact, this monad is pretty much an elaborate wrapper around this
+    idiom:
+
+    .. code-block:: Python
+       from random import randint
+       def none_safe_inc(x):
+           if x is None:
+               return None
+           else:
+               return x+1
+
+       def none_safe_sq(x):
+           if x is None:
+               return None
+           else:
+               return x*x
+
+        def bad_get_int():
+            x = randint(1,10)
+            return x if x%2 else None
+
+       x = bad_get_int()
+       y = none_safe_inc(none_safe_mul(x))
+
+    However, instead of writing `if x is None: return None` a bunch of times,
+    the same block can be rewritten as:
+
+    .. code-block:: Python
+       from pynads import Maybe, Just
+
+       def inc(x): return Just(x+1)
+       def sqr(x): return Just(x*x)
+
+       y = (bad_get_int & Maybe) >> sqr >> inc
+
+    If `bad_get_int` returns None, Maybe, by default, translates None into
+    Nothing (which is implemented as a Singleton, sorry). Nothing propagates
+    itself over binds, mappings and applications. However, if `bad_get_int`
+    returns a value, it's wrapped in Just which will allow operations to
+    take effect on the value.
     """
     __slots__ = ()
 
