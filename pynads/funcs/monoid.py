eca3ca951060b6600317505bc152e7b6e3ea7a60
justanr <alecreiter@gmail.com>
2015-04-06 10:54:50 -0400

    Delegate to correct function in pynads.funcs.monoid.mconcat

--- pynads/funcs/monoid.py
+++ pynads/funcs/monoid.py
@@ -2,15 +2,16 @@
 *are* monoidal but exist as basic types in Python.
 """
 
-from ..utils.compat import reduce
-from ..utils.monoidal import (get_generic_mappend, get_generic_mempty, 
+
+from ..utils.monoidal import (get_generic_mappend, get_generic_mempty,
                               generic_mconcat, is_monoid)
 
-__all__ = ('mempty', 'mappend', 'mconcat')
 
-    
+__all__ = ('mempty', 'mappend', 'mconcat', 'is_monoid')
+
+
 def mempty(monoid, **kwargs):
-    """Returns the mempty value of a Monoid. 
+    """Returns the mempty value of a Monoid.
 
     If the object passed is an implementation of pynads.abc.Monoid, then the
     mempty attribute found there is returned. Otherwise, if it fulfills certain
@@ -38,10 +39,10 @@ def mappend(existing, other, **kwargs):
     uses the mappend method on the object. Otherwise, if it fulfills certain
     criteria, then a generic mappend can be determined and that is used
     instead. This criteria is determined by pynads.utils.get_generic_mappend.
-    
+
     Sequences -- e.g. lists and tuples -- that aren't strings are returned as
     lists.
-    
+
     If neither case is true, then a TypeError is raised (propagated from
     get_generic_mappend).
 
@@ -69,21 +70,19 @@ def mconcat(*monoids, **kwargs):
 
     Sequences -- e.g. lists and tuples -- that aren't strings are returned as
     lists.
-    
+
     If neither case is true,
     then a TypeError is raised (propagated from get_generic_mappend.
 
-    It is possible to pass optional keyword arguments to get_generic_mappend
+    It is possible to pass optional keyword arguments to generic_mappend
     by using kwargs.
 
     See:
         pynads.utils.monoidal.get_generic_mappend
         pynads.utils.monoidal.generic_mconcat
     """
-
     if hasattr(monoids[0], 'mconcat'):
         mconcat_ = monoids[0].mconcat
         return mconcat_(*monoids)
     else:
-        mappend = get_generic_mappend(monoids[0], **kwargs)
-        return generic_mconcat(mappend, *monoids)
+        return generic_mconcat(*monoids, **kwargs)
