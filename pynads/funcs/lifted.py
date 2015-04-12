d691fe54a47a1fee718be99e7599572b15b5268a
justanr <alecreiter@gmail.com>
2015-04-12 15:56:20 -0400

    Use new annotate decorator and added a few useful functions in pynads.funcs.lifted

--- pynads/funcs/lifted.py
+++ pynads/funcs/lifted.py
@@ -1,21 +1,30 @@
 from functools import reduce
 from operator import mul, rshift
+from ..concrete.list import List
+from ..utils.decorators import annotate
+from .monoid import mappend
+from .pure import identity
 
 
-__all__ = ('fmap', 'unit', 'lift', 'multiapply', 'multibind')
+__all__ = ('fmap', 'unit', 'lift', 'multiapply', 'multibind'
+           'cons', 'mcons', 'sequence', 'mapM')
 
 
+@annotate(type="Functor f => (a -> b) -> f a -> f b")
 def fmap(f, functor):
-    """Callable form of fmap."""
+    """Functional form of fmap.
+    """
     return functor.fmap(f)
 
 
+@annotate(type="Applicative f => a -> f a")
 def unit(v, applicative):
     """Puts a value in the minimum necessary context to form an Applicative.
     """
     return applicative.unit(v)
 
 
+@annotate(type="Applicative f => f (a -> b) -> [f a] -> f b")
 def multiapply(initial, *args):
     """Shorthand for stringing many applicatives together.
 
@@ -25,12 +34,14 @@ def multiapply(initial, *args):
     return reduce(mul, args, initial)
 
 
-def lift(f, v, monad):
-    """Applies a function to a value and puts it into a monadic context.
+@annotate(type="Applicative f => (a -> b) -> a -> f -> f b")
+def lift(f, v, applicative):
+    """Applies a function to a value and puts it into an applicative context.
     """
-    return unit(f(v), monad)
+    return unit(f(v), applicative)
 
 
+@annotate(type="Monad m => m a -> [a -> m a] -> m a")
 def multibind(monad, *binds):
     """Shortcut function for composing many monadic binds together.
 
@@ -40,3 +51,58 @@ def multibind(monad, *binds):
 
     """
     return reduce(rshift, binds, monad)
+
+
+@annotate(type="a -> [a] -> [a]")
+def cons(x, xs):
+    """Prepends a value to a pynads.List of existing values.
+
+    .. code-block:: Python
+        from pynads import List
+        from pynads.funcs import cons
+
+        cons(1, List(2,3)) # List(1,2,3)
+        cons(1, cons(2, cons(3, List()))) # List(1,2,3)
+    """
+    return mappend(List(x), xs)
+
+
+@annotate(type="Monad m => m a -> m [a] -> m [a]")
+def mcons(p, q):
+    """Prepends a monadic value to a pynads.List of values inside of a monad.
+
+    .. code-block:: python
+        from pynads import Just
+        from pynads.funcs import mcons
+
+        mcons(Just(1), Just(List(2,3)))   # Just(List(1,2,3))
+        mcons(Nothing, Just(List(1,2,3))) # Nothing
+    """
+    return p >> (lambda x:
+                 q >> (lambda xs:
+                       p.unit(cons(x, xs))))
+
+
+@annotate(type="Monad m => [m a] -> m [a]")
+def sequence(*monads):
+    """Folds a list of monads into a monad containing a list of the
+    monadic values.
+
+    .. code-block:: python
+        justs = List(*[Just(x) for x in range(5)])
+        sequence(*justs)
+        # In: List(Just 0, Just 2, Just 3, Just 4)
+        # Just List(0, 1, 2, 3, 4)
+    """
+    return reduce(lambda q, p: mcons(p, q),
+                  reversed(monads),
+                  monads[0].unit(List()))
+
+
+@annotate(type="Moand m => (a -> m b) -> [a] -> m [b]")
+def mapM(func, *xs):
+    """Maps a monadic function over an iterable of plain values, lifting
+    each into a monadic context then uses sequence to convert the iterable
+    of monads into a monad containing a pynads.List of values.
+    """
+    return sequence(*map(func, xs))
--- tests/test_funcs_lifted.py
+++ tests/test_funcs_lifted.py
@@ -1,4 +1,4 @@
-from pynads import Just
+from pynads import Just, List, Nothing
 from pynads.funcs import lifted
 
 add_two = lambda x: x+2
@@ -27,3 +27,25 @@ def test_multibind():
     j = Just.unit(2)
     assert lifted.multibind(j, m_add_two, m_add_two) == Just(6)
 
+
+def test_cons():
+    assert lifted.cons(1, List(2,3)) == List(1,2,3)
+    assert lifted.cons(1, 
+               lifted.cons(2, 
+                   lifted.cons(3, 
+                       lifted.cons(4, List())))) == List(1,2,3,4)
+
+
+def test_mcons():
+    assert lifted.mcons(Just(0), Just(List(1,2,3))) == Just(List(0,1,2,3))
+    assert lifted.mcons(Nothing, Just(List(0))) is Nothing
+
+
+def test_sequence():
+    justs = [Just(x) for x in range(5)]
+    assert lifted.sequence(*justs) == Just(List(0,1,2,3,4))
+    assert lifted.sequence(Nothing, *justs) is Nothing
+
+
+def test_mapM():
+    assert lifted.mapM(m_add_two, *range(5)) == Just(List(2,3,4,5,6))
