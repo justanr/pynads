95d554e138fb43eb57b770ee271a6af340659f89
justanr <alecreiter@gmail.com>
2015-05-01 09:40:36 -0400

    Must have slipped by when I restaged a commit.

--- pynads/concrete/__init__.py
+++ pynads/concrete/__init__.py
@@ -4,7 +4,6 @@ from .list import List
 from .maybe import Maybe, Just, Nothing
 from .map import Map
 from .mempty import Mempty
-from . import monoids
 from .reader import Reader, Function, Reader as R  # provide shortcut
 from .state import State
 from .writer import Writer
